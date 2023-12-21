1e8eb98a07b24c39266f8c14453971308c279f4d
Nicholas Grinage <ngrinage13@gmail.com>
2023-12-21 14:08:51 -0500

    register js

---
+++ account/register.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const registerButton = document.querySelector("#registerButton")
+const username = document.querySelector("#username")
+const fullName = document.querySelector("#fullName")
+const password = document.querySelector("#password")
+
+function formToUser() {
+    let user = {
+        username: username.value,
+        fullName: fullName.value,
+        password: password.value,
+    };
+    return user;
+}
+
+function addUser() {
+    const user = formToUser();
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then(function () {
+      window.location.href = "register.html";
+    });
+  }
+
+
+registerButton.onclick = addUser;
