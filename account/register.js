1bca3e19f7cb04b2913239155a261b6968c949f5
Nicholas Grinage <ngrinage13@gmail.com>
2023-12-22 15:40:05 -0500

    start of viewposts html & js

--- account/register.js
+++ account/register.js
@@ -23,7 +23,7 @@ function addUser() {
       },
       body: JSON.stringify(user),
     }).then(function () {
-      window.location.href = "register.html";
+      window.location.href = "register.html"; 
     });
   }
 
