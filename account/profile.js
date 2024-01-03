1d8781b2b4bad82d32a8b22f70935f94d70b0d22
DevFaulk <faulkdev@gmail.com>
2024-01-03 15:30:42 -0500

    kait, christina, teddy style profile page

--- account/profile.js
+++ account/profile.js
@@ -2,3 +2,21 @@
 if (isLoggedIn() === false) {
   window.location.replace("../account/register.html");
 }
+
+// Modal Functionality
+
+let editProfile = document.getElementById("editProfile");
+const cancelButton = document.getElementById("cancel");
+
+let createPostModal = document.getElementById("createPostModal");
+let closeModal = document.querySelector(".close");
+
+editProfile.onclick = function () {
+  createPostModal.style.display = "block";
+};
+closeModal.onclick = function () {
+  createPostModal.style.display = "none";
+};
+cancelButton.onclick = function () {
+  createPostModal.style.display = "none";
+};
