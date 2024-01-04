ec7f7977549e59d338f4593fc438238b6e63cc3f
DevFaulk <faulkdev@gmail.com>
2024-01-03 23:27:56 -0500

    style navbar

--- index.js
+++ index.js
@@ -17,6 +17,31 @@ const profileLogo = document.getElementById("profileLogo");
 const appLogo = document.getElementById("appLogo");
 const profileOverText = document.getElementById("profileOverText");
 const homeHoverText = document.getElementById("homeHoverText");
+function toggleProfileHoverText(toggle) {
+  if (toggle) {
+    profileOverText.style.display = "inline-block";
+  } else {
+    profileOverText.style.display = "none";
+  }
+}
 
-profileLogo.onmouseover = profileOverText.style.display = "inline";
-appLogo.onmouseover = homeHoverText.style.display = "inline";
+function toggleHomeHoverText(toggle) {
+  if (toggle) {
+    homeHoverText.style.display = "inline-block";
+  } else {
+    homeHoverText.style.display = "none";
+  }
+}
+
+profileLogo.onmouseover = function () {
+  toggleProfileHoverText(true);
+};
+profileLogo.onmouseout = function () {
+  toggleProfileHoverText();
+};
+appLogo.onmouseover = function () {
+  toggleHomeHoverText(true);
+};
+appLogo.onmouseout = function () {
+  toggleHomeHoverText();
+};
