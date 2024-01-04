5c680c3bcfabcba8a90b43d4645d5a0e2129e024
DevFaulk <faulkdev@gmail.com>
2024-01-04 05:02:07 -0500

    style posts

--- posts/view-posts.js
+++ posts/view-posts.js
@@ -15,21 +15,42 @@ function loadAllPosts() {
     .then((posts) => {
       userPosts.innerHTML = "";
       for (let post of posts) {
-        console.log(post)
         let card = document.createElement("div");
         card.className = "card user-post m-3";
 
-        let usersName = document.createElement("h3");
-        usersName.innerText = post.username;
-        usersName.className = "card-title p-3";
+        let usersName = document.createElement("em");
+        usersName.innerText = `@${post.username}`;
+        usersName.className = "card-title p-3 text-light-emphasis";
 
+        let usersContent = document.createElement("div");
+        usersContent.className = "card-body post-content d-flex justify-content-between p-3";
         let usersMessage = document.createElement("p");
         usersMessage.innerText = post.text;
         usersMessage.className = "card-text post-content p-3";
+        let usersPhoto = document.createElement("div");
+        usersPhoto.className = "photo";
 
+        const imageUrlRegex = /:\s*(.+)/;
+        const match = post.text.match(imageUrlRegex);
+
+        if (match) {
+          // If an image URL is found, create an image element
+          // console.log(match)
+          const imageUrl = match[1];
+          const imageElement = document.createElement("img");
+          imageElement.className = "post-img card-img p-1 m-3 shadow";
+          imageElement.src = `https:${imageUrl}`;
+          usersPhoto.appendChild(imageElement);
+
+          // Display the text without the image URL
+          usersMessage.innerText = post.text.replace(imageUrlRegex, "");
+        } else {
+          // If no image URL is found, use the original post text
+          usersMessage.innerText = post.text;
+        }
         let likeContent = document.createElement("div");
         likeContent.className =
-          "content d-flex p-1 ps-0 pe-2 m-3 justify-content-evenly align-items-center";
+          "content d-flex p-1 ps-0 pe-2 m-3 justify-content-evenly align-items-center align-self-end";
         let heartButton = document.createElement("span");
         heartButton.className = "heart d-flex align-self-stretch";
         let likeAndNumb = document.createElement("span");
@@ -40,8 +61,12 @@ function loadAllPosts() {
         let likeNumb = document.createElement("span");
         likeNumb.className = "numb p-1";
 
+        
+
         card.appendChild(usersName);
-        card.appendChild(usersMessage);
+        card.appendChild(usersContent);
+        usersContent.appendChild(usersMessage);
+        usersContent.appendChild(usersPhoto)
         card.appendChild(likeContent);
         likeContent.appendChild(heartButton);
         likeContent.appendChild(likeAndNumb);
@@ -62,3 +87,6 @@ function loadAllPosts() {
 // Function Calls
 
 loadAllPosts();
+
+// ! uncomment in future
+// setTimeout (LoadAllPost(), 3000)
